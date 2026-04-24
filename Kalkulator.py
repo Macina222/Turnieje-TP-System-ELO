71cf7682f4aa349748ff48f98c1447e03d03e596
Maciej Marek Zych <01206616@pw.edu.pl>
2026-04-24 17:34:45 +0200

    Initial commit

---
+++ Kalkulator.py
@@ -0,0 +1,36 @@
+def oblicz_oczekiwane_elo(rating_a, rating_b):
+    return 1 / (1 + 10 ** ((rating_b - rating_a) / 400))
+
+def aktualizacja_rankingu(lista_par, wskaznik_k):
+    """
+        lista_par: lista słowników w formacie [{'couple': obiekt_Para, 'place': int}]
+        """
+    n = len(lista_par)
+    if n < 2:
+        return
+
+    zmiany = [0] * n
+
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+
+            expected = oblicz_oczekiwane_elo(lista_par[i]['couple'].rating, lista_par[j]['couple'].rating)
+
+            place_i = lista_par[i]['place']
+            place_j = lista_par[j]['place']
+
+            # Logika punktacji: mniejsze miejsce = wygrana
+            if place_i < place_j:
+                actual = 1.0  # Para 'i' wygrała
+            elif place_i > place_j:
+                actual = 0.0  # Para 'i' przegrała
+            else:
+                actual = 0.5  # Remis (zajęli to samo miejsce)
+
+            zmiany[i] += (actual - expected)
+
+    efektywne_k = wskaznik_k / (n-1)
+    for i in range(n):
+        lista_par[i]['couple'].rating += zmiany[i] * efektywne_k
---
+++ Para.py
@@ -0,0 +1,4 @@
+class Para:
+    def __init__(self, tancerz, tancerka, rating=1200):
+        self.name = f"{tancerka} & {tancerz}"
+        self.rating = rating
