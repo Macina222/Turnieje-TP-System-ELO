b633db7dd98b22bd5e86c75e7a6aff1f0c4e1e3e
Maciej Marek Zych <01206616@pw.edu.pl>
2026-04-24 21:46:05 +0200

    Data scraper, processing turniurjów

---
+++ Para.py
@@ -0,0 +1,13 @@
+class Para:
+    def __init__(self, tancerz1, tancerz2, elo = 1200.0):
+        self.tancerz1 = tancerz1
+        self.tancerz2 = tancerz2
+        self.elo = elo
+
+    def pobierz_id(self):
+        # Tuple (krotka) jest idealna jako unikalny identyfikator pary
+        return self.tancerz1, self.tancerz2
+
+    # Dodatkowa metoda __str__, aby ładnie wyświetlać informacje o obiekcie
+    def __str__(self):
+        return f"Para: {self.tancerz1} i {self.tancerz2} (Ranking: {self.elo})"
